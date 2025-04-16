430489c736f837a7e3861aba51b42a416adc1ccc
Vinay <vinaykumar.koudi@smarterp.com>
2025-04-16 14:17:45 +0530

    Api key commented

--- lib/tools/common.ts
+++ lib/tools/common.ts
@@ -2,14 +2,14 @@ import { tool } from "@langchain/core/tools";
 import axios from 'axios';
 import { z } from "zod";
 const sgMail = require('@sendgrid/mail')
-const EmailKey = process.env.SENDGRID_API_KEY ?? `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`;
+const EmailKey = process.env.SENDGRID_API_KEY ?? ``; // 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
 sgMail.setApiKey(EmailKey);
 
 
 export const getWeather = tool(
     async ({ query, location = 'Bangalore, KA' }) => {
         try {
-            const API_KEY = process.env.WEATHER_API_KEY ?? ``;
+            const API_KEY = process.env.WEATHER_API_KEY ?? ``; // z1jBwV4Rz8obuHXlb2gJws74K8ay6Ue2
             const API = process.env.WEATHER_API ?? `https://api.tomorrow.io/v4/weather/realtime`;
             let config = {
                 method: 'GET',
